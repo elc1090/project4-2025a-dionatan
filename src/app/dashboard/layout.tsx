b4467fa16bab06be322a8d207b83073ab3507426
Dionatan Rodrigues <48455774+Dionatan2019Rodrigues@users.noreply.github.com>
2025-06-24 03:33:54 -0300

    ♻️ refactor : add cursor-pointer in the signOut button

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -54,7 +54,7 @@ const DashboardLayout = async ({ children }: { children: ReactNode }) => {
           <form action={signOut}>
             <button
               type="submit"
-              className="bg-red-500 text-white px-4 py-2 rounded-md hover:bg-red-600 flex items-center"
+              className="bg-red-500 cursor-pointer text-white px-4 py-2 rounded-md hover:bg-red-600 flex items-center"
             >
               <svg
                 className="w-5 h-5 mr-2"
